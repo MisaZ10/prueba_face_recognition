8b9ae7be97a61d08183dffdc35581ccb89e31704
misael <misaelzurdo@gmail.com>
2018-04-30 19:16:18 -0300

    use detectFaces

--- addFaces.js
+++ addFaces.js
@@ -1,26 +1,63 @@
+const async = require('async');
 const fr = require('face-recognition');
 const path = require('path');
 const fs = require('fs');
 const recognizer = fr.AsyncFaceRecognizer();
-
-// const dataPath = path.resolve('./robert');
-// const allFiles = fs.readdirSync(dataPath)
-//                 .map(f => path.join(dataPath, f));
-// const allImgs = allFiles.map(image => fr.loadImage(image));
-// recognizer.addFaces(allImgs, 'robert')
-// .then(() => {
-//     const modelState = recognizer.serialize();
-//     fs.writeFileSync('robert-model.json', JSON.stringify(modelState))
-// });
-
+const detector = fr.AsyncFaceDetector();
 
 const modelState = require('./robert-model.json')
-recognizer.load(modelState)
-const faceImg = fr.loadImage('./obama.jpg');
-recognizer.predictBest(faceImg)
-.then((prediction) => {
-    console.log(prediction.className, prediction.distance)
-}).catch((err) => {
-    console.log('predictBest: ', err);
-});
+recognizer.load(modelState);
 
+function checkObamaFace(){
+    const faceImg = fr.loadImage('./obama-face.png');
+    recognizer.predictBest(faceImg)
+    .then((prediction) => {
+        console.log(prediction.className, prediction.distance)
+    }).catch((err) => {
+        console.log('predictBest: ', err);
+    });
+}
+function addFaces(pathDir){
+   return new Promise((resolve, reject) => {
+        const dataPath = path.resolve(pathDir);
+        const allFiles = fs.readdirSync(dataPath)
+                        .map(f => path.join(dataPath, f));
+        const allImgs = allFiles.map(image => fr.loadImage(image));
+        let i = 0;
+        const detectFace = (img, next) => {
+            i++;
+            detector.detectFaces(img)
+            .then((faceImages) => {
+                if(faceImages && faceImages[0]) {
+                    fr.saveImage(pathDir + `-face/${i}.png`, faceImages[0]);
+                    next();
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                next();
+            });
+        }
+        const endFaces = () => {
+            console.log("Fin procesar imagenes de " + pathDir);
+            resolve();
+        }
+        async.eachSeries(allImgs, detectFace, endFaces)
+    });
+}
+function createModel(pathDir, classname) {
+    addFaces(pathDir).then(() => {
+        const dataPath = path.resolve(pathDir + '-face');
+        const allFiles = fs.readdirSync(dataPath)
+                        .map(f => path.join(dataPath, f));
+        const allImgs = allFiles.map(image => fr.loadImage(image));
+        recognizer.addFaces(allImgs, classname)
+        .then(() => {
+            const modelState = recognizer.serialize();
+            fs.writeFileSync(classname + '-model.json', JSON.stringify(modelState))
+        });
+
+    });
+}
+// createModel('./robert', 'robert')
+checkObamaFace();
